93d8e6ff94f6c12dc1680b26bace360d0de9b809
kakikubo <kakikubo@gmail.com>
2021-09-14 06:31:10 +0900

    nullとundefinedという型を定義(あまり使わない)

---
+++ src/null-and-undefined.ts
@@ -0,0 +1,13 @@
+export {};
+
+// 型推論では any になる
+// let absence = null;
+// null型をもたせる
+let absence: null = null;
+// absence = 'Hello';
+console.log(absence);
+
+// undefinedというデータ型
+let data: undefined = undefined;
+
+console.log(data);
