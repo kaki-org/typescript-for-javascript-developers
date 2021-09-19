fb6e364a4fa28a2ec67ea034e6c385f02f0fca92
kakikubo <kakikubo@gmail.com>
2021-09-19 15:39:37 +0900

    overload signature

---
+++ src/overloads.ts
@@ -0,0 +1,19 @@
+export {};
+
+// signatureと呼ぶ(overload)
+function double(value: number): number;
+function double(value: string): string;
+
+function double(value: any): any {
+  console.log(typeof value);
+  if (typeof value === 'number'){
+    return value * 2;
+  } else { // (typeof value === 'string') { 
+    return value + value;
+  }
+}
+
+console.log(double(100));
+console.log(double("Go! "));
+// console.log(double(true)); // これはsignatureでちゃんと弾いてくれる
+
