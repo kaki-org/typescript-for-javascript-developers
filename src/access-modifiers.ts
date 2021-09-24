a44c8ba2cc67571ae20d0067747e8fcfd0bf98dd
kakikubo <kakikubo@gmail.com>
2021-09-24 16:40:17 +0900

    アクセス修飾子を使ってみる

---
+++ src/access-modifiers.ts
@@ -0,0 +1,34 @@
+export {};
+
+class Person {
+  public name: string;
+  // private age: number;
+  protected age: number;
+  protected nationality: string;
+
+  // コンストラクタの戻り値はreturnしないので型の指定をしない
+  constructor(name: string, age: number, nationality: string) {
+    this.name = name;
+    this.age = age;
+    this.nationality = nationality;
+  }
+
+  profile(): string {
+    return `name: ${this.name}, age: ${this.age}`;
+  }
+}
+class Android extends Person {
+  constructor(name: string, age: number, nationality: string) {
+    super(name, age, nationality);
+  }
+
+  profile(): string {
+    return `name: ${this.name}, age: ${this.age}, nationality: ${this.nationality}`;
+  }
+}
+
+let taro = new Android('Taro', 30, 'Japan');
+console.log(taro.profile());
+console.log(taro.name);
+// console.log(taro.age);
+// let hanako = new Person();
