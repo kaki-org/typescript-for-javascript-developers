02d0aeeefb31cb453f5b2361cd1005a6f05d2d04
kakikubo <kakikubo@gmail.com>
2021-09-24 17:09:17 +0900

    コンストラクタを使い倒す

---
+++ src/more-constructor.ts
@@ -0,0 +1,8 @@
+export {};
+
+class Person { // 以下の通りアクセス修飾子をコンストラクタの引数に定義してあげると、省略表記を使うことができる
+  constructor(public name: string, protected age: number){}
+}
+
+const me = new Person('はむさん', 43);
+console.log(me);
