5db142261236fcfaa211f8f600ac153742fe2c67
kakikubo <kakikubo@gmail.com>
2021-09-25 10:07:06 +0900

    prettierで整形してみた

--- src/functions/overloads.ts
+++ src/functions/overloads.ts
@@ -6,14 +6,14 @@ function double(value: string): string;
 
 function double(value: any): any {
   console.log(typeof value);
-  if (typeof value === 'number'){
+  if (typeof value === 'number') {
     return value * 2;
-  } else { // (typeof value === 'string') { 
+  } else {
+    // (typeof value === 'string') {
     return value + value;
   }
 }
 
 console.log(double(100));
-console.log(double("Go! "));
+console.log(double('Go! '));
 // console.log(double(true)); // これはsignatureでちゃんと弾いてくれる
-
