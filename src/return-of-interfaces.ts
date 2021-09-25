808ccdc0eca43b7c22d6b9129327b63e2798bd65
kakikubo <kakikubo@gmail.com>
2021-09-25 10:06:48 +0900

    インターフェースを使った実装

---
+++ src/return-of-interfaces.ts
@@ -0,0 +1,28 @@
+export {};
+
+class Mahotsukai {}
+class Souryo {}
+
+class Taro extends Mahotsukai {} // 単一継承
+
+interface Kenja {
+  ionazun(): void;
+}
+
+interface Senshi {
+  kougeki(): void;
+}
+
+class Jiro implements Kenja, Senshi {
+  // 複数実装は可能
+  ionazun(): void {
+    console.log('ionazun');
+  }
+  kougeki(): void {
+    console.log('kougeki');
+  }
+}
+
+let jiro = new Jiro();
+jiro.ionazun();
+jiro.kougeki();
