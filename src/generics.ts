164a388d43a8840e4571ab2e7c9df53f55480556
kakikubo <kakikubo@gmail.com>
2021-09-29 06:48:48 +0900

    ジェネリクスについて学ぶ

---
+++ src/generics.ts
@@ -0,0 +1,31 @@
+export {};
+
+// これらをうまく書けないか
+// const echo = (arg: number): number => {
+//   return arg;
+// };
+// const echo = (arg: string): string => {
+//   return arg;
+// };
+
+const echo = <T>(arg: T): T => {
+  return arg;
+};
+export default echo;
+
+console.log(echo<number>(100));
+console.log(echo<string>('kakikubo'));
+console.log(echo<boolean>(true));
+console.log(echo<number>(123));
+
+class Mirror<T> {
+  constructor(public value: T) {}
+
+  echo(): T {
+    return this.value;
+  }
+}
+
+console.log(new Mirror<number>(123).echo());
+console.log(new Mirror<string>('Hello').echo());
+console.log(new Mirror<boolean>(true).echo());
---
+++ tests/generics.test.ts
@@ -0,0 +1,20 @@
+import { expectType } from 'tsd';
+import echo from '../src/generics';
+
+describe('echo', (): void => {
+  test('numberを指定したときはnumberが返る', (): void => {
+    const result: number = echo<number>(123);
+    expect(result).toEqual(123);
+    expectType<number>(result);
+  });
+  test('stringを指定したときはstringが返る', (): void => {
+    const result: string = echo<string>('Hello');
+    expect(result).toEqual('Hello');
+    expectType<string>(result);
+  });
+  // test.skip('stringを指定したときはエラーが返る', (): void => {
+  //   expect(() => {
+  //     echo<number>('Hello');
+  //   }).toThrow();
+  // });
+});
