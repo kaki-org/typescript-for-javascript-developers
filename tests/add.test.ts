6938e61c62803d322aed6f6d51fbfca830612df8
kakikubo <kakikubo@gmail.com>
2021-10-01 06:57:12 +0900

    move directory

--- tests/add.test.ts
+++ tests/add.test.ts
@@ -1,8 +1,8 @@
-import add from '../src/high-types/add'
+import add from '../src/class-types/add';
 
 describe('add', (): void => {
   test('should return 3', (): void => {
-    const result: number = add(1, 2)
-    expect(result).toEqual(3)
-  })
-})
+    const result: number = add(1, 2);
+    expect(result).toEqual(3);
+  });
+});
