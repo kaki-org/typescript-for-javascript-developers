f93b89d7f70594a3a0020eff6d14e0dd9ef3a32d
kakikubo <kakikubo@gmail.com>
2021-10-03 07:05:44 +0900

    exclude extract non-nullable

---
+++ src/exclude-extract-nonullable.ts
@@ -0,0 +1,14 @@
+export {};
+
+type DebugType = () => void;
+type SomeTypes = string | number | DebugType;
+type FunctionType = Exclude<SomeTypes, string | number>;
+type NonFunctionType = Exclude<SomeTypes, DebugType>;
+type TypeExcludingFunction = Exclude<SomeTypes, Function>; // こういう書き方もできる
+
+type FuntionTypeByExtract = Extract<SomeTypes, DebugType>;
+type NonFunctionTypeByExtract = Extract<SomeTypes, string | number>;
+type FunctionTypeExtractingFunction = Extract<SomeTypes, Function>;
+
+type NullableTypes = string | number | null | undefined;
+type NonNullableTypes = NonNullable<NullableTypes>;
