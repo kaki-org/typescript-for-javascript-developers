c4db55333ea5fe79830588b49c4c1c9cf43883be
kakikubo <kakikubo@gmail.com>
2021-10-09 07:33:49 +0900

    数値、真偽値に対するテストを追加

--- tests/sum.test.ts
+++ tests/sum.test.ts
@@ -1,3 +1,4 @@
+import exp from 'constants';
 import sum from '../src/sum';
 
 describe('sum', (): void => {
@@ -19,5 +20,39 @@ describe('matcher', () => {
         expect(a + b).not.toBe(0);
       }
     }
+  });
+  // 真偽値
+  test('null', () => {
+    const n: null = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+  });
+  test('zero', () => {
+    const z: number = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+  });
+  // 数値
+  test('two plus two', () => {
+    const value: number = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+
+    // toBeとtoEqualは同じ
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
+  });
+  test('adding floating point numbers', () => {
+    const value: number = 0.1 + 0.2;
+    // expect(value).toBe(0.3); // 丸め込み誤差
+    expect(value).toBeCloseTo(0.3);
   });
 });
